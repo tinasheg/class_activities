7ba9a740060d63b88490c71b25dbc3b5d6a8633e
tinashe <giyavha_tinashe@berkeley.edu>
2019-10-19 10:35:02 -0700

    Adds review

---
+++ week 5/review/javascript.js
@@ -0,0 +1,13 @@
+const gameDiv = document.getElementById("game-div");
+gameDiv.textContent = "Test!"
+
+
+
+let x = [1,2,3,4, "r", 2];
+
+for (let index = 0; index < x.length; index++) {
+    const element = x[index];
+    console.log(x[index], index)
+};
+
+
