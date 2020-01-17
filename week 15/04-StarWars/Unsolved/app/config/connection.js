a27fdd14162fed2e98c32fb2a5e51d74a82d9ba0
tinashe <giyavha_tinashe@berkeley.edu>
2020-01-16 20:56:11 -0800

    Star wars solved

--- week 15/04-StarWars/Unsolved/app/config/connection.js
+++ week 15/04-StarWars/Unsolved/app/config/connection.js
@@ -2,19 +2,20 @@
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
 // *********************************************************************************
 
-// Requiring mysql package
-var mysql = require("mysql");
 
 // Setting up our connection information
-var source = {
-  localhost: {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "starwars"
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize("starwars", "root", "root", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
   }
-};
+})
 
 
 // Creating our connection
