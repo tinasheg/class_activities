e3063a7cfbfd57d3c132730f49f0015c68882d1b
tinashe <giyavha_tinashe@berkeley.edu>
2020-03-16 18:46:37 -0700

    Adds activity solutions

---
+++ week 20/09-Stu_AJAXBooks/Unsolved/client/src/pages/Books.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import DeleteBtn from "../components/DeleteBtn";
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
+
+class Books extends Component {
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    synopsis: ""
+  };
+
+  componentDidMount() {
+    this.loadBooks();
+  }
+
+  loadBooks = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.state({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { title, author, synopsis } = this.state;
+    API.saveBook({
+      author,
+      synopsis,
+      title
+    }).then(this.loadBooks())
+  }
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <Jumbotron>
+              <h1>What Books Should I Read?</h1>
+            </Jumbotron>
+            <form>
+              <Input name="title" placeholder="Title (required)" onChange={this.handleChange} value={this.state.title} />
+              <Input name="author" placeholder="Author (required)" onChange={this.handleChange} value={this.state.author} />
+              <TextArea name="synopsis" placeholder="Synopsis (Optional)" onChange={this.handleChange} value={this.state.synopsis} />
+              <FormBtn onClick={this.handleSubmit}>Submit Book</FormBtn>
+            </form>
+          </Col>
+          <Col size="md-6 sm-12">
+            <Jumbotron>
+              <h1>Books On My List</h1>
+            </Jumbotron>
+            {this.state.books.length ? (
+              <List>
+                {this.state.books.map(book => {
+                  return (
+                    <ListItem key={book._id}>
+                      <a href={"/books/" + book._id}>
+                        <strong>
+                          {book.title} by {book.author}
+                        </strong>
+                      </a>
+                      <DeleteBtn />
+                    </ListItem>
+                  );
+                })}
+              </List>
+            ) : (
+                <h3>No Results to Display</h3>
+              )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Books;
